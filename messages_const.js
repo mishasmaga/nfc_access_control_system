dd7e38dea016fd9aec3bb9002c2180694e582104
Mihail Smaga <Mihail.Smaga@zeppelin.com>
2018-12-09 21:46:41 +0200

    add logic for processing controller tag

--- messages_const.js
+++ messages_const.js
@@ -1,9 +1,11 @@
 module.exports = {
     user_not_found : "user not found",
     user_found: "user found",
-    insert_key:"insert key",
+    insert_key_tag:"insert key tag",
     key_approved:"key approved",
     key_not_approved:"key not approved",
+    any_key:"any kay",
+    not_all_keys:"not all keys",
     controller_not_found:"controller not found",
     done: "done!",
     tag_not_found:"tag not found",
--- routes.js
+++ routes.js
@@ -11,52 +11,96 @@ module.exports = function(app, db) {
       var keysCollection = dbo.collection("keys");
       var poolCollection = dbo.collection("pool");
       var historyCollection = dbo.collection("history");
-
-     // console.log(req);
-     // console.log(params);
-      console.log(id);
-
-      //var user = await userCollection.find().toArray();
-
-      res.send("@"+responseMessages.tag_not_found+"@");
+     
+      //res.send("@"+responseMessages.tag_not_found+"@");
       
-      // //check tag is user     
-      // var user = await userCollection.find({tagid:id});
-      // if(user!=null){
-      //   if(user.isController){
-      //     //get last pool 
-      //     var poolRecords = await poolCollection.find().toArray();
+       //check tag is user     
+      var user = await userCollection.find({tagid:id});
+      if(user!=null){
+        if(user.isController){
+         
+            var historyRecord = await historyCollection.findOne({intransaction:true});
+            if(historyRecord==null){
+              //error
+              res.send(convertToArduinoMessage(responseMessages.error));
+            }
+            else{
+                //first part
+                if(historyRecord.controller1==null ){
+                  if(historyRecord.keys1.length==0){
+                    //error
+                    res.send(convertToArduinoMessage(responseMessages.any_key));
+                  }else{
+                    //sign history record
+                    historyRecord.date_start = DateTime.Now();
+                    historyRecord.controller1 = new DBRef('users', user._id);
+                    historyRecord.intransaction = false;
+                   
+                    historyCollection.save(historyRecord);
 
-      //     if(poolRecords.length = 0 ){
-      //       //error -> close con
-      //       res.send({mess:responseMessages.error});
-      //     }
-      //     else{
-      //       var firstPoolRecord = poolRecords[0];
-      //     //check history
+                    res.send(convertToArduinoMessage(responseMessages.done));
+                  }
+                }
+                else{
+                  //check key1 to key2 collections
+                  var isEqual = true;
+
+                  if(isEqual){
+                    //sign and close trans
+                    historyRecord.date_end = DateTime.Now();
+                    historyRecord.controller2 = new DBRef('users', user._id);
+                    historyRecord.intransaction = false;
+
+                    historyCollection.save(historyRecord);
+
+                    res.send(convertToArduinoMessage(responseMessages.done));
+
+                  }else{
+                    //show error
+                    res.send(convertToArduinoMessage(responseMessages.not_all_keys))
+                  }
+                }
+            }
+          }
+        
+        else{
+          //check history for not closed transaction (keys not on user)           
+          //var h = await historyCollection.findOne({userid: user._id, controller1:{$e:null}});
+          // if(h!=null){
+
+          // }
+
+          //create new history record
+          var h = await historyCollection.save({user_id:new DBRef('users',user._id)});
+          res.send(convertToArduinoMessage(responseMessages.insert_key_tag));
+        }
+      }
+      else{
+        //check tag is key
+        var key = await keysCollection.find({tagid:id});
+        if(key!=null){
+          //add to history
           
+          //1 get history without 1 controller
 
-      //     }
-          
-
-      //   }else{
-      //     //check pool and add
-           
-          
-      //   }
-      // }
-      // else{
-      //   //check tag is key
-      //   var key = await keysCollection.find({tagid:id});
-      //   if(key!=null){
-      //     //check pool and add to pool
-
-      //   }else{
-      //     //tag not found
-      //     res.send({mess:responseMessages.tag_not_found});
-      //   }
-      // }  
-      
-      
+          var history = await historyCollection.findOne({controller1:null});
+          if(history == null){
+            res.send(convertToArduinoMessage(responseMessages.error));
+          }else{
+            //2 check user permissions
+            //3 set key to this history and send approved
+            //3.1 send not approved   
+            
+            var user = await userCollection.findOne({'_id':})
+          } 
+        }else{
+          //tag not found
+          res.send(convertToArduinoMessage(responseMessages.tag_not_found));
+        }
+      }            
     });
   };
+
+  function convertToArduinoMessage(m){
+      return "@"+m+"@";
+  }
