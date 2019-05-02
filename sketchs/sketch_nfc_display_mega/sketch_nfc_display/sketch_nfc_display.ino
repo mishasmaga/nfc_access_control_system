9670216bf47ab454b94bdd7e48674abfdfc013a3
Mihail Smaga <Mihail.Smaga@zeppelin.com>
2019-05-02 22:36:41 +0300

    add logic for sync type using ntp

--- sketchs/sketch_nfc_display_mega/sketch_nfc_display/sketch_nfc_display.ino
+++ sketchs/sketch_nfc_display_mega/sketch_nfc_display/sketch_nfc_display.ino
@@ -43,6 +43,7 @@ byte mac[] = {
     0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
 IPAddress ip(192, 168, 0, 102);
 IPAddress myDns(192, 168, 0, 1);
+IPAddress timeServer(128, 138, 140, 44 ); // time.nist.gov
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
@@ -98,12 +99,13 @@ void setup()
   }
 
   //time synchronization inits
+  setSyncInterval(10); // Set seconds between re-sync
+  setSyncProvider(getNtpTime);   //sync type using ntp
   setSyncProvider(RTC.get);   // the function to get the time from the RTC
   if(timeStatus()!= timeSet) 
      Serial.println("Unable to sync with the RTC");
   else
      Serial.println("RTC has set the system time");      
-
 
   Serial.print("setup end");
   delay(1000);
@@ -206,4 +208,44 @@ string printDigits(int digits){
   result += digits;
 
   return result;
+}
+
+/*-------- NTP code ----------*/
+
+unsigned long getNtpTime()
+{
+  sendNTPpacket(timeServer); // send an NTP packet to a time server
+    delay(500);
+  
+    if ( Udp.parsePacket() ) {
+     Udp.read(packetBuffer,NTP_PACKET_SIZE);
+     unsigned long hi = word(packetBuffer[40], packetBuffer[41]);
+     unsigned long low = word(packetBuffer[42], packetBuffer[43]);
+     unsigned long secsSince1900 = hi << 16 | low; 
+     const unsigned long seventyYears = 2208988800UL;     
+     unsigned long epoch = secsSince1900 - seventyYears;
+     return epoch;
+  }
+  return 0; // return 0 if unable to get the time
+} 
+
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);  // set all bytes in the buffer to 0  
+
+  // Initialize values needed to form NTP request
+  packetBuffer[0] = B11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum
+  packetBuffer[2] = 6;     // Max Interval between messages in seconds
+  packetBuffer[3] = 0xEC;  // Clock Precision
+  // bytes 4 - 11 are for Root Delay and Dispersion and were set to 0 by memset
+  packetBuffer[12]  = 49;  // four-byte reference ID identifying
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52; 
+
+  // send the packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer,NTP_PACKET_SIZE);
+  Udp.endPacket();
 }
